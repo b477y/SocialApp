2116891c50dcece796d40e2cbd9c52a714e77618
Mohammed Saleh <mohammedsaleh002@outlook.com>
2025-01-28 14:49:54 +0200

    Add initial error handling to app controller and registration service
    - Implemented error handling logic in app.controller.js and registration.service.js
    - Added utility functions in src/utils/ to support error management

--- src/app.controller.js
+++ src/app.controller.js
@@ -1,22 +1,24 @@
 import connectDB from "./db/connection.js";
 import authController from "./modules/auth/auth.controller.js";
+import { globalErrorHandling } from "./utils/response/error.response.js";
 
 const bootstrap = (app, express) => {
-  app.use(express.json());  
+  app.use(express.json());
 
-  
   app.get("/", (req, res, next) => {
     return res.status(200).json({ message: "hello" });
   });
-  
+
   app.use("/auth", authController);
-  
+
   app.all("*", (req, res, next) => {
     return res.status(404).json({ message: "not found" });
   });
 
+  app.use(globalErrorHandling);
+
   // db connection
-  connectDB()
+  connectDB();
 };
 
 export default bootstrap;
--- src/modules/auth/services/registration.service.js
+++ src/modules/auth/services/registration.service.js
@@ -1,5 +1,7 @@
-export const signup = (req, res, next) => {
+import { asyncHandler } from "../../../utils/response/error.response.js";
+
+export const signup = asyncHandler(async (req, res, next) => {
   const { username, email, password, confirmationPassword } = req.body;
   const data = { username, email, password };
   return res.status(200).json({ message: "Signup" });
-};
+});
